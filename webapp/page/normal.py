1b2f348cf30f47cc016034e7af6c129d5b9983ec
AlexLeungZ <alexleungchunwai@gmail.com>
2024-10-30 00:29:25 +0800

    Fix bug for game resetting

--- webapp/page/normal.py
+++ webapp/page/normal.py
@@ -33,15 +33,19 @@ class Status:
 
 @unique
 class Message(StrEnum):
-    win = "Congratulations, you guessed the word!"
+    win = "Congratulations! The word was"
     loss = "Sorry, you've used all rounds. The word was"
 
+    def message(self: Self, word: str) -> tuple[str, str]:
+        return self.name, f"{self.value} - {word}."
 
+
+# Convert form data with name: key0, key1,..., key5 to list
 def get_list(data: dict[str, str], keys: str, length: int) -> list[str]:
     return [data[key] for i in range(length) if (key := f"{keys}{i}") in data]
 
 
-# Redirect to main
+# Route for the normal mode
 @bp.route("/", methods=["GET"])
 def home() -> ResponseReturnValue:
     return render_template(
@@ -49,6 +53,7 @@ def home() -> ResponseReturnValue:
     )
 
 
+# Route for receiving player input and update the game status
 @bp.route("/form", methods=["POST"])
 def form() -> ResponseReturnValue:
     status = Status(app.config.get("ROUNDS", 6))
@@ -57,21 +62,32 @@ def form() -> ResponseReturnValue:
     attempt = request.form.get("attempt")
     message = None
 
+    # If the attempt is valid and not exceeding the number of attempts
     if attempt and (attempt := int(attempt)) < status.attempts:
+        # If the input word is valid
         if value := word_checker(keyArr):
+            result = wordle(value, status.target)
             status.guess[attempt] = value
-            status.result[attempt] = wordle(value, status.target)
+            status.result[attempt] = result
             attempt += 1
 
+        # If player wins the game
         if value == status.target:
-            message = (msg := Message.win).name, msg.value
+            message = Message.win.message(status.target)
+            status.reset()
+            attempt = None
+
+        # If player loses the game
         elif attempt >= status.attempts:
-            message = (msg := Message.loss).name, f"{msg.value} - {status.target}"
+            message = Message.loss.message(status.target)
+            status.reset()
+            attempt = None
 
     else:
         status.reset()
         attempt = None
 
+    # Logging the correct answer and guess history to the logger
     app.logger.info(f"Current target: {status.target}")
     app.logger.info(f"Guess history: {status.guess}")
 
